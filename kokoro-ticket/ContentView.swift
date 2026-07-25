253cf6a8605d9aac7a4c4fdee19d0b54ff852ef6
haruki328 <y.haruki328@gmail.com>
2026-07-25 22:44:12 +0900

    chore: SwiftUIプロジェクト初期構築

---
+++ kokoro-ticket/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  kokoro-ticket
+//
+//  Created by 山本悠生 on 2026/07/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ kokoro-ticket/kokoro_ticketApp.swift
@@ -0,0 +1,17 @@
+//
+//  kokoro_ticketApp.swift
+//  kokoro-ticket
+//
+//  Created by 山本悠生 on 2026/07/25.
+//
+
+import SwiftUI
+
+@main
+struct kokoro_ticketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
